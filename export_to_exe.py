e1d951d3ce489e3884c276a9f2251a149761fe9c
Wojciech Kowal <w.kowal202@gmail.com>
2023-05-09 14:58:45 +0200

    Add assets.txt to exe export

--- export_to_exe.py
+++ export_to_exe.py
@@ -4,7 +4,9 @@ import cx_Freeze
 # generate Windows installer: python export_to_exe.py bdist_msi
 
 packages = ["pygame"]
-include_files = [("fonts", "fonts"), ("images", "images"), ("LICENSE", "BirdJumper_license.txt")]
+include_files = [("fonts", "fonts"), ("images", "images"), ("LICENSE", "BirdJumper_license.txt"),
+                 ("assets.txt", "assets_sources.txt")]
+
 # base="Win32GUI" so the console window does not pop up
 executables = [cx_Freeze.Executable("main.py", base="Win32GUI", icon="images/duck-ga9276d9c3_640.ico",
                                     target_name="BirdJumper.exe")]
